02bceab39790eeb45557913a3e19430a8bd7db0b
Arman Ansari <armanansari8426@gmail.com>
2026-06-12 12:50:23 +0530

    Updated code by deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/next';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -47,6 +48,7 @@ export default function App() {
     <ThemeProvider>
       <BrowserRouter>
         <AppLayout>
+          <Analytics />
           <Routes>
             {/* Public Pages */}
             <Route path="/" element={<LandingPage />} />
